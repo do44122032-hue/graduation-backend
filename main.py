e6b7d00bc9fc579bd9daab487211a92e764dd9fa
Dania osama <do44122032@cihanuniversity.edu.iq>
2026-03-20 13:48:07 -0700

    Fix CORS for Chrome

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Graduation Project API (PostgreSQL)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
