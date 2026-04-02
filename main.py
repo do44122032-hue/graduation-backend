f852d0081074272e01a3c4c4c376c5bb9bf020a0
Dania osama <do44122032@cihanuniversity.edu.iq>
2026-04-02 09:42:31 -0700

    Final CORS fix for Browser

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Graduation Project API (PostgreSQL)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"],
